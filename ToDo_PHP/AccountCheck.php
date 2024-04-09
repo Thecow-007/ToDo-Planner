dfa14a9343b797ef19592eff7a0d36f00f8326c1
CFort0366 <fort0366@algonquinlive.com>
2024-04-08 20:36:32 -0400

    broken but new

--- ToDo_PHP/AccountCheck.php
+++ ToDo_PHP/AccountCheck.php
@@ -1,22 +1,36 @@
+
+
 <?php 
+
 require_once 'db_connection.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["pass"];
     $password2 = $_POST["pass2"];
 
+    
+function thePHP() {
+    logTest();
+
+$conn = openConnection();
+
     if (strlen($username) <= 20 && !empty($username)) {
+        $validUsername = true;
         echo '<script>';
         echo 'validUsername();';
         echo '</script>';
+        
     }
             if (empty($username)) {
+            $validUsername = false;
             echo '<script>';
             echo 'mustIncludeUsername();';
             echo '</script>';
             exit;
         }
             if (strlen($username) > 20) {
+            $validUsername = false;
             echo '<script>';
             echo 'userMustBeLessThan20char();';
             echo '</script>';
@@ -25,12 +39,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     if (strlen($password) > 6 && !empty($password) && preg_match('/^(?=.*[a-z])(?=.*[A-Z]).{6,}$/', $password)) {
+        $validPassword = true;
         echo '<script>';
         echo 'validPassword();';
         echo '</script>';
     } 
 
             if (strlen($password) < 6 || !preg_match('/^(?=.*[a-z])(?=.*[A-Z]).{6,}$/', $password)) {
+            $validPassword = false;
             echo '<script>';
             echo 'passMustInclude();';
             echo '</script>';
@@ -39,17 +55,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 
     if (strlen($password2) > 6 && $password == $password2) {
+        $valid2ndPass = true;
         echo '<script>';
         echo 'valid2ndPass();';
         echo '</script>';
     }
             if (empty($password2)) {
+            $valid2ndPass = false;
             echo '<script>';
             echo 'mustRepeatPassword();';
             echo '</script>';
             exit;
         }
             if (strlen($password2) < 6 || $password !== $password2) {
+            $valid2ndPass = false;
             echo '<script>';
             echo 'passDoNotMatch();';
             echo '</script>';
@@ -63,12 +82,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $userCheck->execute();
     $result = $userCheck->get_result();
 
-    // if ($result->num_rows > 0) {
-    //     echo '<script>';
-    //     echo 'userAlreadyExists();';
-    //     echo '</script>';
-    //     exit;
-    // }
+    if ($result->num_rows > 0) {
+        $userIsNew = false;
+        echo '<script>';
+        echo 'userAlreadyExists();';
+        echo '</script>';
+        exit;
+    } else {
+        $userIsNew = true;
+    }
 
     $hashedPassword = hash('sha256', $password);
 
@@ -76,8 +98,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $userCheck = $connection->prepare($insertToUser);
     $userCheck->bind_param(":userName", $username);
     $userCheck->bind_param(":passWord", $hashedPassword);
-    
-    closeCon();
-}
 
+    closeCon();
+
+    return $validUsername && $validPassword && $valid2ndPass && $userIsNew;
+}
+}
 ?>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDo: Login</title>
-    <script src="./login.js"></script>
+    <script src="../ToDo_JS\login.js"></script>
     <link rel="stylesheet" href="./ToDo_CSS/login.css">
 </head>
 
